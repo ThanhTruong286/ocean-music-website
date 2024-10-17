6a1f0d89bcb165ec0cefe6e68fcec50db744c703
ThanhTruong286 <caot43069@gmail.com>
2024-10-17 21:41:48 +0700

    add sidebar into home

---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+
+const Home = () => {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
+export {
+    Home
+}
