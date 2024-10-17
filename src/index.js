da34bb6ca26d8faac78533d70f417aa6f853192f
ThanhTruong286 <caot43069@gmail.com>
2024-10-17 21:45:29 +0700

    del default comments

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './views/App';
 import reportWebVitals from './reportWebVitals';
-import './styles/global.scss'
+import './styles/global.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,7 +18,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
