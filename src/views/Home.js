c34c0cd671d6bd162a41aa786b00ec5bea2cbd73
ThanhTruong286 <caot43069@gmail.com>
2024-10-18 16:59:49 +0700

    put header into home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,9 @@
 import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
 
 const Home = () => {
     return (
         <div>
+            <Header />
         </div>
     )
 }
