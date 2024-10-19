c1d1b4ca62ee22dea2c2b332bae466718871661f
ThanhTruong286 <caot43069@gmail.com>
2024-10-19 15:34:28 +0700

    add backend side

---
+++ backend/controllers/authController.js
@@ -0,0 +1,54 @@
+// controllers/authController.js
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+
+// Đăng ký người dùng mới
+const registerUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Kiểm tra xem người dùng đã tồn tại
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists.' });
+    }
+
+    // Mã hóa mật khẩu
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ email, password: hashedPassword });
+
+    await newUser.save();
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// Đăng nhập người dùng
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid email or password.' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid email or password.' });
+    }
+
+    const token = jwt.sign({ id: user._id }, config.JWT_SECRET, { expiresIn: '1h' });
+    res.status(200).json({ token, userId: user._id });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
