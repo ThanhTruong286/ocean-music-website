f594290d6b62f05ac1f5e5844325f09dda2a6ae5
ThanhTruong286 <caot43069@gmail.com>
2024-10-19 18:00:45 +0700

    update

--- frontend/src/views/App.js
+++ frontend/src/views/App.js
@@ -8,19 +8,14 @@ import Album from './Album';
 const App = () => {
   return (
     <div id='main'>
-      
+
       <aside className="sidebar sidebar-base" id="first-tour" data-toggle="main-sidebar">
         <Sidebar />
       </aside>
       <main class="main-content">
         <Album />
       </main>
-<<<<<<< HEAD:src/views/App.js
-      <Footer />
-  
-=======
       {/* <Footer /> */}
->>>>>>> dev:frontend/src/views/App.js
     </div>
   )
 }
