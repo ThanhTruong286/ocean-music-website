7a7ac07b0e97dcdccc8b777c9a1dce4ac663c842
ThanhTruong286 <caot43069@gmail.com>
2024-10-19 14:28:56 +0700

    add recent played and trending songs in home page

--- src/views/App.js
+++ src/views/App.js
@@ -6,13 +6,14 @@ import Footer from '../components/Footer';
 const App = () => {
   return (
     <div id='main'>
+      
       <aside className="sidebar sidebar-base" id="first-tour" data-toggle="main-sidebar">
         <Sidebar />
       </aside>
       <main class="main-content">
         <Home />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
