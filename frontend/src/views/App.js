ee85ef5cf4324a8c7421c639d63a621cb583febd
ThanhTruong286 <caot43069@gmail.com>
2024-10-20 12:32:45 +0700

    fix DOM errors

--- frontend/src/views/App.js
+++ frontend/src/views/App.js
@@ -6,11 +6,11 @@ import Footer from '../components/Footer';
 const App = () => {
   return (
     <div id='main'>
-      
+
       <aside className="sidebar sidebar-base" id="first-tour" data-toggle="main-sidebar">
         <Sidebar />
       </aside>
-      <main class="main-content">
+      <main className="main-content">
         <Home />
       </main>
       {/* <Footer /> */}
