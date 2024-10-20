aa1fa7947d1a990a36f8e3c7977a9d4b869da642
ThanhTruong286 <caot43069@gmail.com>
2024-10-20 18:35:07 +0700

    add swiper to artist list

--- frontend/src/components/ArtistList.js
+++ frontend/src/components/ArtistList.js
@@ -4,6 +4,11 @@ import peanut from "../assets/images/artists/peanut.jpg";
 import faker from "../assets/images/artists/faker.jpg";
 import React, { useEffect, useState } from 'react';
 import { fetchArtists } from '../api/api';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
 
 const ArtistList = () => {
     const [artists, setArtists] = useState([]);
@@ -34,26 +39,34 @@ const ArtistList = () => {
     }
 
     return (
-        <div className="row artist-list">
-            {artists.map((artist) => (
-                <div className="col-xl-3 col-md-6 mb-3" key={artist.artist_id}> {/* Sử dụng artist.artist_id làm key */}
-                    <div className="bg-soft-danger position-relative rounded-3 card-box mb-3">
-                        <img
-                            src={artist.image || peanut}
-                            id="artist-playlist"
-                            className="img-fluid mx-auto d-block"
-                            alt="play-img"
-                        />
-                    </div>
-                    <a href="../dashboard/music-player.html" className="text-capitalize h5">{artist.name}</a>
-                    <small className="fw-normal line-count-1 text-capitalize">
-                        <b style={{ color: artist.color || '#F05A22' }}>{artist.tag || 'Tag'}</b> {artist.bio || 'Nickname'}
-                    </small>
-                </div>
-            ))}
+        <div className="artist-list">
+            <Swiper
+                spaceBetween={30}  // Giảm khoảng cách giữa các slides
+                slidesPerView={5}  // Hiển thị 5 slides cùng lúc
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                {artists.map((artist) => (
+                    <SwiperSlide key={artist.artist_id}> {/* Đặt mỗi artist vào một SwiperSlide */}
+                        <div className="artist-card"> {/* Thêm class name cho từng khối */}
+                            <div className="bg-soft-danger position-relative rounded-3 card-box mb-3">
+                                <img
+                                    src={artist.image || peanut}
+                                    id="artist-playlist"
+                                    className="img-fluid mx-auto d-block"
+                                    alt="play-img"
+                                />
+                            </div>
+                            <a href="../dashboard/music-player.html" className="text-capitalize h5">{artist.name}</a>
+                            <small className="fw-normal line-count-1 text-capitalize">
+                                <b style={{ color: artist.color || '#F05A22' }}>{artist.tag || 'Tag'}</b> {artist.bio || 'Nickname'}
+                            </small>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </div>
     );
-
 }
 
 export default ArtistList;
