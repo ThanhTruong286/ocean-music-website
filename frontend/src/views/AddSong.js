c09223f6dc1704cf503f370b4c2b4756b7c55f21
makabe1302 <86347174+makabe1302@users.noreply.github.com>
2024-11-15 13:26:04 +0700

    cap nhan giao dien addsong
    cap nhan giao dien addsong

--- frontend/src/views/AddSong.js
+++ frontend/src/views/AddSong.js
@@ -1,9 +1,15 @@
+import Header from "../components/Header";
+
+import Sidebar from "../components/Sidebar";
+
+import Footer from "../components/Footer";
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { fetchAddNewSong } from '../api/api'; // Import API để thêm bài hát
+import { fetchAddNewSong } from '../api/api';
 import '../styles/addsong.scss';
+// Import Swiper React components
 
-const AddSong = () => {
+const Home = () => {
   const [newSong, setNewSong] = useState({
     image: '',
     title: '',
@@ -11,11 +17,10 @@ const AddSong = () => {
     writer: '',
     singer: '',
   });
-  const [loading, setLoading] = useState(false);  // Thêm trạng thái loading để theo dõi quá trình gửi dữ liệu
-  const [error, setError] = useState(null); // Thêm trạng thái lỗi nếu có
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const navigate = useNavigate();
 
-  // Hàm xử lý sự kiện thay đổi giá trị input
   const handleChange = (e) => {
     const { name, value } = e.target;
     setNewSong((prevState) => ({
@@ -24,79 +29,98 @@ const AddSong = () => {
     }));
   };
 
-  // Hàm xử lý thêm bài hát
   const handleAddSong = async () => {
     try {
-        const result = await fetchAddNewSong(newSong);
-        if (result && result.songId) {
-            navigate('/music-list');
-        } else {
-            setError('Lỗi khi thêm bài hát');
-        }
-    } catch (error) {
-        console.error('Lỗi khi gọi API:', error);
+      const result = await fetchAddNewSong(newSong);
+      if (result && result.songId) {
+        navigate('/music-list');
+      } else {
         setError('Lỗi khi thêm bài hát');
+      }
+    } catch (error) {
+      console.error('Lỗi khi gọi API:', error);
+      setError('Lỗi khi thêm bài hát');
     }
-};
-
+  };
   return (
-    <div className="add-song-container">
-      <h1>Add New Song</h1>
-      {error && <div className="error-message">{error}</div>} {/* Hiển thị lỗi nếu có */}
-      <div className="form-group">
-        <input
-          type="text"
-          name="title"
-          placeholder="Song Name"
-          value={newSong.title}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-group">
-        <input
-          type="text"
-          name="image"
-          placeholder="Image URL"
-          value={newSong.image}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-group">
-        <input
-          type="text"
-          name="genre"
-          placeholder="Genres"
-          value={newSong.genre}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-group">
-        <input
-          type="text"
-          name="writer"
-          placeholder="Writer"
-          value={newSong.writer}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-group">
-        <input
-          type="text"
-          name="singer"
-          placeholder="Singer"
-          value={newSong.singer}
-          onChange={handleChange}
-        />
-      </div>
-      <button
-        className="btn btn-add-song"
-        onClick={handleAddSong}
-        disabled={loading}  // Disable nút khi đang gửi dữ liệu
-      >
-        {loading ? 'Adding...' : 'Add New Song'}
-      </button>
-    </div>
-  );
-};
+    <div>
+      <aside className="sidebar sidebar-base" id="first-tour" data-toggle="main-sidebar">
+        <Sidebar />
+      </aside>
+      <main className="main-content">
+        <div id="home">
+          <Header />
+          <div className="add-song-container">
+            <h1>Add New Song</h1>
+            {error && <div className="error-message">{error}</div>}
 
-export default AddSong;
+            <div className="form-card">
+              <label>Title</label>
+              <input
+                type="text"
+                name="title"
+                placeholder="Song Name"
+                value={newSong.title}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-card">
+              <label>Image URL</label>
+              <input
+                type="text"
+                name="image"
+                placeholder="Image URL"
+                value={newSong.image}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-card">
+              <label>Genre</label>
+              <input
+                type="text"
+                name="genre"
+                placeholder="Genres"
+                value={newSong.genre}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-card">
+              <label>Writer</label>
+              <input
+                type="text"
+                name="writer"
+                placeholder="Writer"
+                value={newSong.writer}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-card">
+              <label>Singer</label>
+              <input
+                type="text"
+                name="singer"
+                placeholder="Singer"
+                value={newSong.singer}
+                onChange={handleChange}
+              />
+            </div>
+
+            <button
+              className="btn"
+              onClick={handleAddSong}
+              disabled={loading}
+            >
+              {loading ? 'Adding...' : 'Add New Song'}
+            </button>
+          </div>
+        </div >
+      </main>
+      <Footer />
+    </div>
+  )
+}
+export default Home
